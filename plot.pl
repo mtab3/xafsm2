ade9d885dda6d20749425c5715e6595219fb8f49
Masao Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-04-26 20:59:24 +0900

    closing

--- plot.pl
+++ plot.pl
@@ -12,7 +12,7 @@ while( $fnames ne "" ) {
     while(<IN>) {
 	chomp;
 	$fname = $_;
-	print OUT "\"" . "$fname" . "\"" . " every ::17 using (12.38/(2.*3.13553*sin(\$2/180*3.1415926535))):(log(\$3/\$4)) with line title '$fname', "
+	print OUT "\"" . "$fname" . "\"" . " every ::17 using (12.38/(2.*3.13553*sin(\$2/180*3.1415926535))):(log(\$4/\$5)) with line title '$fname', "
     }
     
     close( IN );
