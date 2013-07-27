2b5331bf166847acc4a94fb1135d1deb5b594a18
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-07-27 16:46:40 +0900

    gitlog.pl was updated

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "develop2" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Fri Jul 26 14:27:58 2013 +0900\n"\
+                "    the information in gitlog.h is used in Conditions.cpp\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Jul 26 14:21:42 2013 +0900\n"\
                 "    gitlog.pl and gitlog.h was add to the repository\n"\
 		"\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Wed Jul 24 21:01:34 2013 +0900\n"\
                 "    Checking on PZ\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Jul 24 12:50:04 2013 +0900\n"\
-                "    LION\n"\
 		"\n"\
                 ""
 #endif
