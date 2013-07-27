6042d233ca427adba01d80b967abf1f4e9225843
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-07-27 16:47:27 +0900

    nothing

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "develop2" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Jul 27 16:46:40 2013 +0900\n"\
+                "    gitlog.pl was updated\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Jul 26 14:27:58 2013 +0900\n"\
                 "    the information in gitlog.h is used in Conditions.cpp\n"\
 		"\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Jul 25 17:57:03 2013 +0900\n"\
                 "    XAFSM0.def was new\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Jul 24 21:01:34 2013 +0900\n"\
-                "    Checking on PZ\n"\
 		"\n"\
                 ""
 #endif
