f231ccd1726ed4d9c73a86c89498c0d63df86d69
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-08-06 12:29:17 +0900

    do nothing

--- gitlog.h
+++ gitlog.h
@@ -2,27 +2,27 @@
 #define GITLOG_H
 
 
-#define BRANCH_NAME ( "master" )
+#define BRANCH_NAME ( "develop2" )
 #define GITLOGS ""\
+                "Author      : tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Mon Jul 29 17:12:46 2013 +0900\n"\
+                "    The bug which happened when 8th block was used was fixed\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Jul 27 16:49:16 2013 +0900\n"\
+                "    gitlog.pl was updated\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Jul 27 16:47:27 2013 +0900\n"\
+                "    nothing\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Jul 27 16:46:40 2013 +0900\n"\
+                "    gitlog.pl was updated\n"\
+		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Jul 26 14:29:26 2013 +0900\n"\
                 "    master was stampled in gitlog.h\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Fri Jul 26 14:27:58 2013 +0900\n"\
-                "    the information in gitlog.h is used in Conditions.cpp\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Fri Jul 26 14:21:42 2013 +0900\n"\
-                "    gitlog.pl and gitlog.h was add to the repository\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Jul 25 21:13:08 2013 +0900\n"\
-                "    checking QXafs parameter check\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Jul 25 17:57:03 2013 +0900\n"\
-                "    XAFSM0.def was new\n"\
 		"\n"\
                 ""
 #endif
