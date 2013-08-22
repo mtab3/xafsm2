1a0d3fedcfab2de2e7e962404cb3805ba831da6a
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-08-22 20:59:18 +0900

    it looks it works

--- gitlog.h
+++ gitlog.h
@@ -5,23 +5,23 @@
 #define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Aug 22 20:57:43 2013 +0900\n"\
+                "    Merge branch 'develop2'\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Aug 22 20:57:23 2013 +0900\n"\
+                "    Now, Target Atom is input-able.  By typing 1 or 2 letters of the target atom name, the atom is selected.\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Aug 22 20:55:51 2013 +0900\n"\
+                "    Now, Target Atom is input-able.  By typing 1 or 2 letters of the target atom name, the atom is selected.\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Aug 22 19:55:47 2013 +0900\n"\
+                "    merged develop2\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Aug 22 19:55:03 2013 +0900\n"\
-                "    do nothing\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Aug 22 19:54:06 2013 +0900\n"\
-                "    clear some debug messages from AUnit.cpp\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Aug 22 19:40:49 2013 +0900\n"\
-                "    On the QXafs mode the Monochromator must be assigned as CH-A of the 2nd PM16C04\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Aug 6 12:29:42 2013 +0900\n"\
-                "    do nothing\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Aug 6 12:29:17 2013 +0900\n"\
                 "    do nothing\n"\
 		"\n"\
                 ""
