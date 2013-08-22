23a7d28643956fe147e9559ae0ce17ba99703cb4
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-08-22 19:55:03 +0900

    do nothing

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "develop2" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Aug 22 19:54:06 2013 +0900\n"\
+                "    clear some debug messages from AUnit.cpp\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Aug 22 19:40:49 2013 +0900\n"\
                 "    On the QXafs mode the Monochromator must be assigned as CH-A of the 2nd PM16C04\n"\
 		"\n"\
@@ -18,10 +22,6 @@
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Aug 1 19:41:03 2013 +0900\n"\
-                "    do nothing\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Aug 1 19:20:57 2013 +0900\n"\
                 "    do nothing\n"\
 		"\n"\
                 ""
