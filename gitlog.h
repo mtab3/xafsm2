2318de1e3e67b88680f888294d46ae36b2df6663
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-09-25 14:59:53 +0900

    do nothing

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "SSDThreshold" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Sep 24 19:25:19 2013 +0900\n"\
+                "    The lower limit is available for the MCA spectra, but not for the ICR in XAFS measurement\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Sep 19 20:12:18 2013 +0900\n"\
                 "    working on SSDLowLimit\n"\
 		"\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Mon Sep 9 15:22:39 2013 +0900\n"\
                 "    Progress-bar is displaied while QXAFS measurement.   A severe bug that QXafs-on-backward was broken was fixed\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Fri Aug 23 19:54:07 2013 +0900\n"\
-                "    1. The codes treating 'Changer' is tight.   2. The codes were complied with clang and fixed the warned lines.\n"\
 		"\n"\
                 ""
 #endif
