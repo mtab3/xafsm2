fd77b6efd5272901886dec2e0e0f364ce83f7300
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-10-24 11:23:32 +0900

    nothing

--- gitlog.h
+++ gitlog.h
@@ -5,24 +5,24 @@
 #define BRANCH_NAME ( "ContinuousS2DMode" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Oct 19 20:36:40 2013 +0900\n"\
+                "    try to fix a bug in S2DRealContScan, but note sure\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Oct 19 19:56:49 2013 +0900\n"\
+                "    ?\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Oct 17 19:59:25 2013 +0900\n"\
+                "    working on S2D data save after measurement\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Oct 17 18:35:18 2013 +0900\n"\
                 "    GoTo by double-click on Scan screen is available\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Tue Oct 15 22:05:57 2013 +0900\n"\
                 "    S2D was almost done.  The futures, show MCA spectra at pointed position, and change SSD ch with mouse-wheel in S2DView are ready\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Oct 15 01:18:21 2013 +0900\n"\
-                "    S2D mode is almost done.  LION.\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Sun Oct 13 23:09:23 2013 +0900\n"\
-                "    ?\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Sun Oct 13 02:14:38 2013 +0900\n"\
-                "    S2D update map with new ROI is limited only when in S2D\n"\
 		"\n"\
                 ""
 #endif
