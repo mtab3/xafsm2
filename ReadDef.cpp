191da440906bb9a2ccd816cf17b9160bba1b40e2
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-10-30 12:37:54 +0900

    LION of XAFSM.def

--- ReadDef.cpp
+++ ReadDef.cpp
@@ -65,6 +65,24 @@ void MainWindow::ReadDef( QString fname )
 	next = nextItem( next, item ); NewUnit->setCh( item );
 	next = nextItem( next, item ); NewUnit->setUnit( item );
 	NewUnit->setDevCh();
+	// 名前の中の特殊文字の置換
+	// tr でタグ付けされた文字列にしておいて、
+	// lupdate, linguist ペアで ja_JP.qm ファイルを作ることで全角文字に対応
+	QString name = NewUnit->getName();
+	name.replace( QString( "CEY Stg." ),  tr( "CEY" ) );
+	name.replace( QString( "Cap. Stg." ), tr( "CAP" ) );
+	name.replace( QString( "Theta" ),     tr( "Theta!" ) );
+	name.replace( QString( "Dth" ),       tr( "Dth" ) );
+	name.replace( QString( "Sample" ),    tr( "Sample" ) );
+	name.replace( QString( "Stage" ),     tr( "Stage" ) );
+	name.replace( QString( "Stg." ),      tr( "Stage" ) );
+	name.replace( QString( "Phi" ),       tr( "Phi" ) );
+	name.replace( QString( "Omg" ),       tr( "Omg" ) );
+	name.replace( QString( "open" ),      tr( "open" ) );
+	name.replace( QString( "move" ),      tr( "move" ) );
+	name.replace( QString( "Counter" ),   tr( "Counter" ) );
+	NewUnit->setName( name );
+
 	// 以下、motor だけの項目
 	if ( isMotor ) {
 	  // 全 motor 共通
