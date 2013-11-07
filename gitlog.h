19d31945721d8c99cf480962f4359acfe719dbfe
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-11-07 22:45:22 +0900

    no

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "att" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Nov 6 21:17:05 2013 +0900\n"\
+                "    It looks that the data for SSD DT correction can be obtained with the AutoSequence.\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Wed Nov 6 12:59:30 2013 +0900\n"\
                 "    AutoSeq ver.0 is available, with which the data collection for the SSD dead time correct can be done\n"\
 		"\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Tue Nov 5 23:53:52 2013 +0900\n"\
                 "    no\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Nov 5 21:06:59 2013 +0900\n"\
-                "    AutoSeq is working in a part\n"\
 		"\n"\
                 ""
 #endif
