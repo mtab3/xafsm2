17a1853b12902221998d78e6abe77720f9aeb485
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-11-21 20:05:51 +0900

    DEF_U_REAL and ScanParam were merged

--- gitlog.h
+++ gitlog.h
@@ -2,27 +2,27 @@
 #define GITLOG_H
 
 
-#define BRANCH_NAME ( "ScanParam" )
+#define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Nov 21 20:04:41 2013 +0900\n"\
+                "    merged with master\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Nov 21 12:32:45 2013 +0900\n"\
+                "    DEF_U_REAL option is ready\n"\
+		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Wed Nov 20 21:33:41 2013 +0900\n"\
                 "    for 1d Scan 'ScanInfo' is used.   The coding was finished, but not checked.\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Nov 19 17:46:35 2013 +0900\n"\
-                "    DEF_U_REAL line is adding\n"\
+                "Date        : Wed Nov 20 19:27:27 2013 +0900\n"\
+                "    no\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Nov 18 21:25:08 2013 +0900\n"\
-                "    new branch\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Nov 14 16:38:36 2013 +0900\n"\
-                "    removed abnormal files\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Nov 12 20:51:44 2013 +0900\n"\
-                "    energy select in SSD Calibration tab is ready\n"\
+                "Date        : Wed Nov 20 19:22:59 2013 +0900\n"\
+                "    --amend\n"\
 		"\n"\
                 ""
 #endif
