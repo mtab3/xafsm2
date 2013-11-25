0ac924cefa56fd52c64526c4f09e61f145f2a083
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-11-25 19:14:15 +0900

    very small change in GUI

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Mon Nov 25 19:10:35 2013 +0900\n"\
+                "    ROI range is recorded in MCA-record-file\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Nov 22 16:44:29 2013 +0900\n"\
                 "    comment out a debug line from ReadDef.cpp\n"\
 		"\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Nov 21 12:32:45 2013 +0900\n"\
                 "    DEF_U_REAL option is ready\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Nov 20 21:33:41 2013 +0900\n"\
-                "    for 1d Scan 'ScanInfo' is used.   The coding was finished, but not checked.\n"\
 		"\n"\
                 ""
 #endif
