83010d314ac2d866de817c33dc98ea62dfea65a4
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2013-12-16 20:16:44 +0900

    small bug fix on MCASaveOnMem

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "AfterXAFS" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Mon Dec 16 19:28:38 2013 +0900\n"\
+                "    make small fix around MCASaveOnMem\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Mon Dec 16 16:16:21 2013 +0900\n"\
                 "    RecordMCAs was expired and the MCA datas are always saved when the XAFS measurement is done with 19ch-SSD\n"\
 		"\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Dec 6 20:16:49 2013 +0900\n"\
                 "    now S2DView can also pop and dock\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Dec 5 14:17:50 2013 +0900\n"\
-                "    mca data save after a xafs measurement was implemented\n"\
 		"\n"\
                 ""
 #endif
