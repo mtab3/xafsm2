69e78753c18834c55b3696baaea275fb7e547b5e
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2014-05-20 16:48:31 +0900

    style is modified again

--- gitlog.h
+++ gitlog.h
@@ -2,27 +2,27 @@
 #define GITLOG_H
 
 
-#define BRANCH_NAME ( "master" )
+#define BRANCH_NAME ( "UseAio64" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Sun Mar 16 22:41:57 2014 +0900\n"\
-                "    only update WhatToDo\n"\
+                "Date        : Tue May 20 16:15:03 2014 +0900\n"\
+                "    the style sheet is modified\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Sat Mar 15 01:37:03 2014 +0900\n"\
-                "    Fix a bug: XafsM2 crashes running on an environment without SSD\n"\
-		"\n"\
-                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Mar 4 20:56:09 2014 +0900\n"\
-                "    L10N\n"\
+                "Date        : Sun May 18 23:17:04 2014 +0900\n"\
+                "    working on TuneAtEachStep in Measurement.cpp\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Mar 3 20:09:26 2014 +0900\n"\
-                "    QSound is ok\n"\
+                "Date        : Sun May 18 22:39:38 2014 +0900\n"\
+                "    LION\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Mar 3 20:03:41 2014 +0900\n"\
-                "    qt5 available\n"\
+                "Date        : Sun May 18 22:13:02 2014 +0900\n"\
+                "    modify layout of Measurement-Tab adding Tune-at-Each-Step button.\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sun May 18 16:58:32 2014 +0900\n"\
+                "    AUnit is ready for Aio64\n"\
 		"\n"\
                 ""
 #endif
