dd62034dae6f4c9bee5b27627e44a8e0c3b8fcd7
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2014-06-19 19:06:28 +0900

    nothing

--- gitlog.h
+++ gitlog.h
@@ -4,25 +4,25 @@
 
 #define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Jun 18 18:10:13 2014 +0900\n"\
+                "    merged with FP23\n"\
+		"\n"\
+                "Author      : H. Asakura <h.asakura@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Jun 18 15:22:27 2014 +0900\n"\
+                "    Merge branch 'FP23'\n"\"    \n"\"    Conflicts:\n"\"    	AUnit.cpp\n"\"    	AUnit.h\n"\"    	MsgType.cpp\n"\"    	MsgType.h\n"\"    	Stars.cpp\n"\"    	Stars.h\n"\"    	XAFSM.def.BL5S1\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Jun 17 20:10:49 2014 +0900\n"\
+                "    small change\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Sat Jun 14 14:42:56 2014 +0900\n"\
+                "    modify layout of mainwindow\n"\
+		"\n"\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Jun 12 16:35:01 2014 +0900\n"\
                 "    Injection monitor was inplemented\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Jun 11 20:04:23 2014 +0900\n"\
-                "    working on each-step-quick-tuning\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Jun 11 19:27:43 2014 +0900\n"\
-                "    DXM-reset is added in the sequence changing the mode from NORMAL to QXAFS\n"\
-		"\n"\
-                "Author      : H. Asakura <h.asakura@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Dec 26 15:09:14 2013 +0900\n"\
-                "    Fix typo\n"\
-		"\n"\
-                "Author      : H. Asakura <h.asakura@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Dec 26 14:23:07 2013 +0900\n"\
-                "    Change WriteHeaderCore and RecordData\n"\"    \n"\"    For REX2000, and other compatible softwares, the order of recorded data\n"\"    are changed.\n"\
 		"\n"\
                 ""
 #endif
