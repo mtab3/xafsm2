1272c152db09e011ead772cf7e54b5f70a96e5b7
Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>
2014-07-03 15:55:06 +0900

    working on sound but not finished

--- Sounds.cpp
+++ Sounds.cpp
@@ -18,10 +18,36 @@ void MainWindow::PlaySound( QString name, int times )
   QSound sound( name );
   // ここで QSound *sound = new QSound; すると
   // 最後に(or どこかで) delete sound; がペアで必要。
-  // この関数無いでしか使わない実体なら、
+  // この関数内でしか使わない実体なら、
   // local 変数にして new, delete 無しにした方がいい。
 
   sound.setLoops( times );
   sound.play();
+#else
+  // QSound ではなく QtMultimedia を使ってみようかな...
+  // 面倒くさそうなのでまだ使えない
+
+  inputFile.setFileName( name );
+  inputFile.open(QIODevice::ReadOnly);
+  
+  QAudioFormat format;
+  // Set up the format, eg.
+  format.setFrequency(8000);
+  format.setChannels(1);
+  format.setSampleSize(8);
+  format.setCodec("audio/pcm");
+  format.setByteOrder(QAudioFormat::LittleEndian);
+  format.setSampleType(QAudioFormat::UnSignedInt);
+  
+  QAudioDeviceInfo info(QAudioDeviceInfo::defaultOutputDevice());
+  if (!info.isFormatSupported(format)) {
+    qWarning()<<"raw audio format not supported by backend, cannot play audio.";
+    return;
+  }
+  
+  audioOutput = new QAudioOutput(format, this);
+  connect(audioOutput,SIGNAL(stateChanged(QAudio::State)),SLOT(finishedPlaying(QAudio::State)));
+  audioOutput->start(&inputFile);
+
 #endif
 }
