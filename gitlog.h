b2b3a5d599e8602d984c6ce112f45cc7b244d0f2
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2014-11-07 18:26:51 +0900

    another solution of QFio.  I only know both of them can be compiled, but I do not know either way works

--- gitlog.h
+++ gitlog.h
@@ -4,6 +4,10 @@
 
 #define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Fri Nov 7 18:10:31 2014 +0900\n"\
+                "    change somewhere LLVM clang cause error.   It must be checked if this version work in Q-XAFS mode\n"\
+		"\n"\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Jul 3 15:55:06 2014 +0900\n"\
                 "    working on sound but not finished\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Sun Jun 29 22:18:38 2014 +0900\n"\
                 "    small hack, to accept multi spaces in XAFSM.def when they are quoted by double-quotation\n"\
-		"\n"\
-                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Jun 26 17:13:11 2014 +0900\n"\
-                "    L10N\n"\
 		"\n"\
                 ""
 #endif
