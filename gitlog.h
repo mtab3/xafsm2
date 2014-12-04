8df30263a0df7475f4177cc05a5ed3ad8fa502eb
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2014-12-04 18:00:21 +0900

    small bug

--- gitlog.h
+++ gitlog.h
@@ -4,6 +4,10 @@
 
 #define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Dec 4 17:56:42 2014 +0900\n"\
+                "    new XAFSM.def.BL5S1 adding BPM H/V 12/13\n"\
+		"\n"\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Dec 4 16:25:37 2014 +0900\n"\
                 "    small bug fix for EPIC\n"\
@@ -19,10 +23,6 @@
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu Dec 4 14:22:59 2014 +0900\n"\
                 "    new AUnit for EPIC\n"\
-		"\n"\
-                "Author      : H. Asakura <h.asakura@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Dec 1 17:17:47 2014 +0900\n"\
-                "    Fix acquisition time limit in QXAFS mode for typical parameters\n"\
 		"\n"\
                 ""
 #endif
