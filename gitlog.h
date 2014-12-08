4a07b7a25ec162e5b65b4c584446bf195b11e65a
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2014-12-08 15:50:15 +0900

    merged newS2D at a stagi (still working)

--- gitlog.h
+++ gitlog.h
@@ -2,8 +2,12 @@
 #define GITLOG_H
 
 
-#define BRANCH_NAME ( "newS2D" )
+#define BRANCH_NAME ( "master" )
 #define GITLOGS ""\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Mon Dec 8 15:48:36 2014 +0900\n"\
+                "    cBar in S2D is ready\n"\
+		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Sun Dec 7 23:39:23 2014 +0900\n"\
                 "    solved cnfilicts with master\n"\
@@ -19,10 +23,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Sun Dec 7 23:27:04 2014 +0900\n"\
                 "    working on S2DBase and cBar\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Sun Dec 7 00:44:19 2014 +0900\n"\
-                "    working on S2D base\n"\
 		"\n"\
                 ""
 #endif
