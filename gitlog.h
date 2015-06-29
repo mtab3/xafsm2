bf015d635cfe831b39154c64838744445d4a9df7
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2015-06-29 11:23:01 +0900

    nothing

--- gitlog.h
+++ gitlog.h
@@ -2,7 +2,11 @@
 #define GITLOG_H
 
 
+<<<<<<< HEAD
 #define BRANCH_NAME ( "stable" )
+=======
+#define BRANCH_NAME ( "QCt08" )
+>>>>>>> 0618703f6f59bf79a5ef51bf50d3561805d91933
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu May 21 20:47:06 2015 +0900\n"\
@@ -19,6 +23,10 @@
                 "Author      : H. Asakura <h.asakura@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Thu May 21 19:59:00 2015 +0900\n"\
                 "    Update Standard parameters\n"\
+		"\n"\
+                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue May 19 15:14:19 2015 +0900\n"\
+                "    QXafs measurment can do with 4 sensors(with Aux2)\n"\
 		"\n"\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Tue May 19 15:14:19 2015 +0900\n"\
@@ -44,5 +52,24 @@
                 "Date        : Fri May 1 19:44:34 2015 +0900\n"\
                 "    For DV : messages from stars are identified by the condition that 'msg.ToCh() == Uid' in SetCurPos.   The way might be used for all of the messages for all devices\n"\
 		"\n"\
+<<<<<<< HEAD
+=======
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Apr 28 20:07:22 2015 +0900\n"\
+                "    LION\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Apr 28 19:23:47 2015 +0900\n"\
+                "    Block Meas is ready\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Apr 28 17:42:40 2015 +0900\n"\
+                "    working on the slow EIB problem.   DV/DV3 are changed\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Mon Apr 27 18:56:15 2015 +0900\n"\
+                "    FP23 problem is solved. but for CC-10, it has not been checked\n"\
+		"\n"\
+>>>>>>> 0618703f6f59bf79a5ef51bf50d3561805d91933
                 ""
 #endif
