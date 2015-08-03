e27afdd310f1a615b9ab9f9988577fdc416e4a1e
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2015-08-03 12:00:50 +0900

    new XAFSM.defs

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,10 @@
 #define BRANCH_NAME ( "newMwSSD2" )
 #define GITLOGS ""\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Fri Jul 17 17:29:46 2015 +0900\n"\
+                "    merged with origin/newMwSSD2\n"\
+		"\n"\
+                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Jul 17 17:28:24 2015 +0900\n"\
                 "    merged with stable (meas modes)\n"\
 		"\n"\
@@ -15,6 +19,18 @@
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Jul 17 16:27:30 2015 +0900\n"\
                 "    working and broken\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Jul 16 20:11:35 2015 +0900\n"\
+                "    Checking on ICR of SDD\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Jul 16 18:14:43 2015 +0900\n"\
+                "    Merge branch 'newMwSSD2' of ssh://titan/git/Qt0/XAFS/XafsM2 into newMwSSD2\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Thu Jul 16 18:14:39 2015 +0900\n"\
+                "    small change\n"\
 		"\n"\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Tue Jul 14 14:52:50 2015 +0900\n"\
@@ -27,22 +43,6 @@
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Tue Jul 14 14:49:24 2015 +0900\n"\
                 "    'Rh Pb Ag' was corrected 'Rh Pd Ag'\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Jul 13 12:08:52 2015 +0900\n"\
-                "    fixed a bug in s2d\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Jul 13 12:07:36 2015 +0900\n"\
-                "    A bug which crushed XafsM2 when S2D scan was stopped was fixed\n"\
-		"\n"\
-                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Jul 13 11:58:34 2015 +0900\n"\
-                "    finished the merge with stable\n"\
-		"\n"\
-                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Fri Jul 10 17:19:13 2015 +0900\n"\
-                "    merged with stable\n"\
 		"\n"\
                 ""
 #endif
