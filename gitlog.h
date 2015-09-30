a5b5229a60378635f86869cf80ff7df84b69ae01
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2015-09-30 19:23:25 +0900

    It made sure that mon-interval works with real qct08

--- gitlog.h
+++ gitlog.h
@@ -5,12 +5,32 @@
 #define BRANCH_NAME ( "stable" )
 #define GITLOGS ""\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Sep 30 17:36:52 2015 +0900\n"\
+                "    merged with mon-interval\n"\
+		"\n"\
+                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Sep 30 17:34:08 2015 +0900\n"\
+                "    it looks it works with simmotor\n"\
+		"\n"\
+                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Wed Sep 30 17:22:57 2015 +0900\n"\
                 "    fix a bug in MEAS_MODEs.  NSaveNowBlocks was used without initialization\n"\
 		"\n"\
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Wed Sep 30 17:22:11 2015 +0900\n"\
                 "    fix a bug in MEAS_MODEs.  NSaveNowBlocks was used without initialization\n"\
+		"\n"\
+                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Sep 30 17:20:12 2015 +0900\n"\
+                "    fix a bug in MEAS_MODEs\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Wed Sep 30 16:33:52 2015 +0900\n"\
+                "    it may work but not tested\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Mon Sep 28 19:34:26 2015 +0900\n"\
+                "    working\n"\
 		"\n"\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Sep 18 22:04:06 2015 +0900\n"\
@@ -23,26 +43,6 @@
                 "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Fri Sep 18 16:48:59 2015 +0900\n"\
                 "    working on QMeasModes\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Sep 16 19:15:50 2015 +0900\n"\
-                "    Aux1 is selected as default when the measurement mode is changed from normal to quick-xafs mode\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Wed Sep 16 19:07:12 2015 +0900\n"\
-                "    some debug codes\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Aug 3 19:00:21 2015 +0900\n"\
-                "    a bug in S2D StepScan, the measurement time was corrected as if Continuous scan, was fixed\n"\
-		"\n"\
-                "Author      : Masao TABUCHI <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Mon Jul 27 17:40:07 2015 +0900\n"\
-                "    Revival some comments in head-lines of XAFSM.def.BL5S1\n"\
-		"\n"\
-                "Author      : H. Asakura <h.asakura@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Thu Jul 23 19:41:55 2015 +0900\n"\
-                "    Fix writeheadercore on filename\n"\
 		"\n"\
                 ""
 #endif
